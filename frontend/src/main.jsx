7130494b1f47a0a81840ca989a83bedf09878765
mac <mac@macs-MacBook-Pro.local>
2026-08-12 06:56:12 +0530

    Switch frontend routing to HashRouter for production client routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,17 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </Router>
   </StrictMode>
 );
